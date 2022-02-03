094d1070f71e0f072aa210da36ee942d282d6bc6
muratcayir <mrtcyr12@hotmail.com>
2022-02-03 14:58:47 +0300

    pg connection

---
+++ PostgreSQL&Nodejs API/index.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const postgresClient = require("./config/db")
+
+const app = express()
+app.use(express.json())
+
+const PORT=process.env.PORT || 8080
+
+app.listen(PORT,()=>{
+    console.log(`Listening on port ${PORT}`);
+    postgresClient.connect(err=>{
+        if(err){
+            console.log('connection error',err.stack);
+        }
+        else{
+            console.log('db connection successful');
+        }
+    })
+})
